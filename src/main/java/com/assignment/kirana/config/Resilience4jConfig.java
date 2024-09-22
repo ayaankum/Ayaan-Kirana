15a217e18679b2d42916677745d3f646ef4ba70c
ayaan kumar <ayaankumar@example.com>
2024-09-22 17:26:56 +0530

    feat: added rate limiting

---
+++ src/main/java/com/assignment/kirana/config/Resilience4jConfig.java
@@ -0,0 +1,15 @@
+package com.assignment.kirana.config;
+
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Resilience4jConfig{
+
+    @Bean
+    public RateLimiterRegistry rateLimiterRegistry() {
+        return RateLimiterRegistry.ofDefaults();
+    }
+}
